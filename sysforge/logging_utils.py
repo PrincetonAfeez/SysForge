a7b567bce2ff7faa4dc69cb2eef0c2f2b3eda599
Princeton Afeez <princetonafeez@gmail.com>
2026-04-19 19:58:05 -0700

    Get a logger for the given name.

--- sysforge/logging_utils.py
+++ sysforge/logging_utils.py
@@ -14,3 +14,32 @@ def _current_log_level() -> int:
     if os.environ.get("SYSFORGE_VERBOSE") == "1":
         return logging.DEBUG
     return logging.INFO
+
+def get_logger(name: str) -> logging.Logger:
+    ensure_home_layout()
+    logger = logging.getLogger(name)
+    if getattr(logger, "_sysforge_ready", False):
+        logger.setLevel(logging.DEBUG)
+        for handler in logger.handlers:
+            if isinstance(handler, logging.FileHandler):
+                handler.setLevel(logging.DEBUG)
+            else:
+                handler.setLevel(_current_log_level())
+        return logger
+
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
+
+    file_handler = logging.FileHandler(get_central_log_file(), encoding="utf-8")
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(_current_log_level())
+    logger.addHandler(console_handler)
+
+    logger.propagate = False
+    logger._sysforge_ready = True  # type: ignore[attr-defined]
+    return logger
