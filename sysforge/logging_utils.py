f2db183ffb108db451ae4ede6de41024301bb9c1
Princeton Afeez <princetonafeez@gmail.com>
2026-04-19 19:57:16 -0700

    Get the current log level.

--- sysforge/logging_utils.py
+++ sysforge/logging_utils.py
@@ -7,3 +7,10 @@ from pathlib import Path
 
 from sysforge.sysforge_paths import ensure_home_layout, get_central_log_file
 
+
+def _current_log_level() -> int:
+    if os.environ.get("SYSFORGE_QUIET") == "1":
+        return logging.WARNING
+    if os.environ.get("SYSFORGE_VERBOSE") == "1":
+        return logging.DEBUG
+    return logging.INFO
