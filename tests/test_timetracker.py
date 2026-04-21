be09cb335412a93ff87c218c87c6edcaa9662c78
Princeton Afeez <princetonafeez@gmail.com>
2026-04-21 13:39:50 -0700

    Make sure the entry ID has a suffix

--- tests/test_timetracker.py
+++ tests/test_timetracker.py
@@ -26,3 +26,8 @@ def test_seconds_between() -> None:
     a = datetime(2026, 1, 1, 10, 0, tzinfo=UTC)
     b = datetime(2026, 1, 1, 11, 30, tzinfo=UTC)
     assert tt.seconds_between(a, b) == 5400
+
+def test_make_entry_id_has_suffix() -> None:
+    eid = tt.make_entry_id()
+    assert eid.startswith("entry-")
+    assert eid.count("-") >= 2
