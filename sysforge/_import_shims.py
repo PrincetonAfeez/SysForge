e2cd19eba3e2364717f8be78b3b0206f3da9f8eb
Princeton Afeez <princetonafeez@gmail.com>
2026-04-22 10:58:07 -0700

    Compatibility import hooks (see README.md, Changelog)

---
+++ sysforge/_import_shims.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import importlib.util
+import sys
+import types
+import warnings
+from importlib.abc import Loader, MetaPathFinder
+from importlib.machinery import ModuleSpec
+
